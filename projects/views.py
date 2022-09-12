d73de0d53a769f566e6e3e2b564e3e445ef26470
alper ademoglu <alperademoglu@hotmail.com>
2022-09-12 17:30:34 -0400

    Feature 5 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+# from django.contrib.auth.decorators import login_required
+# from receipts.forms import ReceiptForm, ExpenseCategoryForm, AccountForm
+
+
+# username: test_user / test_user2
+# password: sifre123
+
+
+# @login_required
+def ProjectListView(request):
+    context = {"projects": Project.objects.all()}
+
+    return render(request, "projects/list_projects.html", context)
